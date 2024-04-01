e26f5a0b09f1f43a9584cb6464f141a4669a3ae2
Yellow Bear <percussionpeng@gmail.com>
2024-03-31 23:11:11 -0700

    modify MainActivity viewModel instantiate.

--- app/src/main/java/com/example/recyclerviewpractice/ui/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewpractice/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewpractice.R
 import com.example.recyclerviewpractice.adapter.HeaderAdapter
@@ -14,14 +15,19 @@ import com.example.recyclerviewpractice.databinding.ActivityMainBinding
 import com.example.recyclerviewpractice.viewmodel.SnoopyViewModel
 import androidx.recyclerview.widget.ConcatAdapter
 import com.example.recyclerviewpractice.data.Snoopy
+import com.example.recyclerviewpractice.viewmodel.SnoopyViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModelFactory = SnoopyViewModelFactory(this)
+    private val viewModel : SnoopyViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[SnoopyViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
 
-        val viewModel = SnoopyViewModel()
+
         val recyclerView = binding.recyclerview
         val headerAdapter = HeaderAdapter()
         val myAdapter = MyAdapter{ snoopy -> adapterOnClick(snoopy) }
--- app/src/main/java/com/example/recyclerviewpractice/viewmodel/SnoopyViewModel.kt
+++ app/src/main/java/com/example/recyclerviewpractice/viewmodel/SnoopyViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.recyclerviewpractice.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.recyclerviewpractice.R
 import com.example.recyclerviewpractice.data.DataSource
 import com.example.recyclerviewpractice.data.Snoopy
@@ -30,4 +32,16 @@ class SnoopyViewModel(val dataSource: DataSource) : ViewModel() {
         dataSource.addSnoopy(newSnoopy)
     }
 
+}
+class SnoopyViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SnoopyViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SnoopyViewModel(
+                dataSource = DataSource.getDataSource(context.resources)
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
