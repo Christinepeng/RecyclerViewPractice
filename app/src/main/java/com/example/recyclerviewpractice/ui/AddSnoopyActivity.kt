9625cd81924bced0b637cb3664ac391ac81aef38
Yellow Bear <percussionpeng@gmail.com>
2024-04-01 13:21:44 -0700

    finish Add New item function.

--- app/src/main/java/com/example/recyclerviewpractice/ui/AddSnoopyActivity.kt
+++ app/src/main/java/com/example/recyclerviewpractice/ui/AddSnoopyActivity.kt
@@ -3,6 +3,7 @@ package com.example.recyclerviewpractice.ui
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.recyclerviewpractice.R
--- app/src/main/java/com/example/recyclerviewpractice/ui/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewpractice/ui/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.recyclerviewpractice.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewpractice.R
 import com.example.recyclerviewpractice.adapter.HeaderAdapter
 import com.example.recyclerviewpractice.adapter.MyAdapter
@@ -19,6 +17,7 @@ import com.example.recyclerviewpractice.viewmodel.SnoopyViewModelFactory
 
 const val SNOOPY_ID = "snoopy id"
 class MainActivity : AppCompatActivity() {
+    private val newSnoopyActivityRequestCode = 1
     private lateinit var binding: ActivityMainBinding
     private val viewModelFactory = SnoopyViewModelFactory(this)
     private val viewModel : SnoopyViewModel by lazy {
@@ -45,7 +44,6 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.fab.setOnClickListener {
-            Log.e("bear", "setOnClickListener")
             fabOnClick()
         }
     }
@@ -56,9 +54,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fabOnClick() {
-        Log.e("bear", "fabOnClick()")
         val intent = Intent(this, AddSnoopyActivity::class.java)
-//        startActivity(intent)
         startActivityForResult(intent, 1)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        /* Inserts flower into viewModel. */
+        if (requestCode == newSnoopyActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.let { data ->
+                val snoopyName = data.getStringExtra(SNOOPY_NAME)
+                val snoopyDescription = data.getStringExtra(SNOOPY_DESCRIPTION)
+
+                viewModel.insertSnoopy(snoopyName, snoopyDescription)
+            }
+        }
     }
 }
